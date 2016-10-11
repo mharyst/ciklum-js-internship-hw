b2cb8edd998faf5ba28355eea61cb7bea4f87db7
mharyst <mykhailo.haryst@gmail.com>
2016-10-11 03:32:02 +0300

    Added comments functionality

--- lesson6/src/controllers/movie.ctrl.js
+++ lesson6/src/controllers/movie.ctrl.js
@@ -15,12 +15,50 @@ export default class MovieCtrl {
           alert(response.Error);
         } else {
           this.showDetails(response);
+          this.getComments();
         }
       });
   }
 
   showDetails(response) {
     this.movie = response;
+  }
+
+  addComment() {
+    if (this.commentText !== undefined) {
+      this.CommentsService.sendComment(this.commentText, this.movie.imdbID);
+      this.getComments();
+      this.commentText = '';
+    } else {
+      alert("Enter your comment, please");
+    }
+  }
+
+  getComments() {
+    let filmID = this.movie.imdbID;
+    if (localStorage.getItem('comments')) {
+      let respComments = this.CommentsService.getComments();
+      let comments = respComments.filter(function(comment) {
+        let newComment = JSON.parse(comment);
+        if (newComment.id == filmID) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      this.comments = comments.map(function(comment) {
+        let newComment = JSON.parse(comment);
+        return newComment;
+      });
+    } else {
+      this.comments = [];
+    }
+
+  }
+
+  deleteComment(date) {
+    this.CommentsService.deleteComment(date);
+    this.getComments();
   }
 
 }
--- lesson6/src/services/comments.service.js
+++ lesson6/src/services/comments.service.js
@@ -1,3 +1,30 @@
 export default class CommentsService {
-  
+  sendComment(commentText, filmID) {
+    if (localStorage.getItem('comments')) {
+      let comments = localStorage.getItem('comments');
+      this.comments = JSON.parse(comments);
+    } else {
+      this.comments = [];
+    }
+    this.currentComment = {id: filmID, text: commentText, date: Date.now()}
+    this.comments.push(JSON.stringify(this.currentComment));
+    localStorage.setItem('comments', JSON.stringify(this.comments));
+  }
+
+  getComments() {
+    return JSON.parse(localStorage.getItem('comments'));
+  }
+
+  deleteComment(date) {
+    let comments = JSON.parse(localStorage.getItem('comments'));
+    let newComments = comments.filter((comment) => {
+      let newComment = JSON.parse(comment);
+      if (newComment.date == date) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    localStorage.setItem('comments', JSON.stringify(newComments));
+  }
 }
